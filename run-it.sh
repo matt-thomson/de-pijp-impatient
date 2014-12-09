bb2c9fbdfb884dd1c7e0f7b0093fc242eda9e2a9
Matt Thomson <mattjohnthomson@gmail.com>
2014-12-09 16:26:39 +0000

    Refactor script.

--- run-it.sh
+++ run-it.sh
@@ -2,10 +2,8 @@
 /etc/bootstrap.sh
 hdfs dfs -put /opt/de-pijp-impatient/data/ /data
 
-hadoop jar /opt/de-pijp-impatient/target/de-pijp-impatient-jar-with-dependencies.jar io.github.mattthomson.depijp.impatient.Part1 /data/rain.txt /output/part1.tsv
-hadoop jar /opt/de-pijp-impatient/target/de-pijp-impatient-jar-with-dependencies.jar io.github.mattthomson.depijp.impatient.Part2 /data/rain.txt /output/part2.tsv
-hadoop jar /opt/de-pijp-impatient/target/de-pijp-impatient-jar-with-dependencies.jar io.github.mattthomson.depijp.impatient.Part3 /data/rain.txt /output/part3.tsv
-
-hadoop fs -getmerge /output/part1.tsv /opt/de-pijp-impatient/target/part1.tsv
-hadoop fs -getmerge /output/part2.tsv /opt/de-pijp-impatient/target/part2.tsv
-hadoop fs -getmerge /output/part3.tsv /opt/de-pijp-impatient/target/part3.tsv
+for i in 1 2 3
+do
+    hadoop jar /opt/de-pijp-impatient/target/de-pijp-impatient-jar-with-dependencies.jar io.github.mattthomson.depijp.impatient.Part${i} /data/rain.txt /output/part${i}.tsv
+    hadoop fs -getmerge /output/part${i}.tsv /opt/de-pijp-impatient/target/part${i}.tsv
+done
